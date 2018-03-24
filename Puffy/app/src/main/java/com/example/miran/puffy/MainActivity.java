dacf954de26a99e71dc164dce154b849416f7dd6
Diefermireche <37722135+Diefermireche@users.noreply.github.com>
2018-03-23 21:07:55 -0500

    Deber N°11_Material Design Android

---
+++ Puffy/app/src/main/java/com/example/miran/puffy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.miran.puffy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
